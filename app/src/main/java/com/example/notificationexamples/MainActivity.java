52c87b78e3fcaca864a3865759f6953441b51045
unknown <vkksdgn2@gmail.com>
2020-09-13 16:19:44 +0300

    Notification

---
+++ app/src/main/java/com/example/notificationexamples/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.notificationexamples;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static com.example.notificationexamples.App.CHANNEL_1_ID;
+import static com.example.notificationexamples.App.CHANNEL_2_ID;
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+    }
+
+    public void sendOnChannel1(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_looks_one)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+//    public void sendOnChannel2(View v) {
+//        String title = editTextTitle.getText().toString();
+//        String message = editTextMessage.getText().toString();
+//
+//        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+//                .setSmallIcon(R.drawable.ic_looks_two_black_24dp)
+//                .setContentTitle(title)
+//                .setContentText(message)
+//                .setPriority(NotificationCompat.PRIORITY_LOW)
+//                .build();
+//
+//        notificationManager.notify(2, notification);
+//    }
+}
